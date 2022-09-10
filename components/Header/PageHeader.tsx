23981c3fa31b82ad88172751d79f08ba880a1431
abmah <ab.mahasnh@gmail.com>
2022-09-11 01:57:14 +0300

    first commit

---
+++ components/Header/PageHeader.tsx
@@ -0,0 +1,52 @@
+import styles from "./Header.module.css";
+import { useEffect, useState } from "react";
+
+export default function PageHeader() {
+  const [shouldHeaderMinimize, setShouldHeaderMinimize] = useState(false);
+  useEffect(() => {
+    window.onscroll = function () {
+      scrollFunction();
+    };
+  }, []);
+
+  function scrollFunction() {
+    if (document.documentElement.scrollTop > 200) {
+      // activate
+
+      setShouldHeaderMinimize(true);
+    }
+    if (document.documentElement.scrollTop < 20) {
+      setShouldHeaderMinimize(false);
+    }
+  }
+
+  return (
+    <div className={styles.header}>
+      <div className={styles.NameAndTitle}>
+        <div
+          className={
+            shouldHeaderMinimize
+              ? styles.HeaderNameContainerSmall
+              : styles.HeaderNameContainer
+          }
+        >
+          <span className={styles.HeaderName}>A</span>
+          <span className={styles.HeaderName}>.</span>
+          <span className={styles.HeaderName}>B</span>
+          <span className={styles.HeaderName}>.</span>
+        </div>
+        <div className={styles.HeaderTitle}>full-stack developer</div>
+      </div>
+      <div
+        className={
+          shouldHeaderMinimize ? styles.HeaderMenuSmall : styles.HeaderMenu
+        }
+      >
+        <ul className={styles.MenuList}>
+          <li>projects</li>
+          <li>contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
