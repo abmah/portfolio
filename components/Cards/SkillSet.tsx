a115b06b406a568bcf635f46e3dba8b1465f5124
abmah <ab.mahasnh@gmail.com>
2022-09-12 11:57:23 +0300

    bugs

--- components/Cards/SkillSet.tsx
+++ components/Cards/SkillSet.tsx
@@ -14,21 +14,21 @@ export default function SkillSet() {
   const { ref: skillsTitle, inView } = useInView();
   const { ref: skillItemOne, inView: skillItemOneInView } = useInView();
 
-  const [shouldHeaderMinimize, setShouldHeaderMinimize] = useState(0);
+  // const [shouldHeaderMinimize, setShouldHeaderMinimize] = useState(0);
 
-  useEffect(() => {
-    window.onscroll = function () {
-      scrollFunction();
-    };
-  }, []);
+  // useEffect(() => {
+  //   window.onscroll = function () {
+  //     scrollFunction();
+  //   };
+  // }, []);
 
-  function scrollFunction() {
-    const distanceFromTop = Math.floor(document.documentElement.scrollTop / 10);
-    if (distanceFromTop > 80) {
-      setShouldHeaderMinimize(distanceFromTop);
-    }
-  }
-  console.log(shouldHeaderMinimize);
+  // function scrollFunction() {
+  //   const distanceFromTop = Math.floor(document.documentElement.scrollTop / 10);
+  //   if (distanceFromTop > 80) {
+  //     setShouldHeaderMinimize(distanceFromTop);
+  //   }
+  // }
+  // console.log(shouldHeaderMinimize);
 
   return (
     <div className={styles.skills}>
