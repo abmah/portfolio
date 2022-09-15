ebf6114f777a2f57291ef105af2dfe77e5e0c374
abmah <ab.mahasnh@gmail.com>
2022-09-15 14:46:36 +0300

    bugs

--- components/Test/TestPage.tsx
+++ components/Test/TestPage.tsx
@@ -1,29 +1,5 @@
-import React, { useEffect } from "react";
-import { setInterval } from "timers/promises";
-import styles from "./Test.module.css";
-export default function TestPage() {
-  const [x, setX] = React.useState(0);
-  const [y, setY] = React.useState(0);
-  // useEffect(() => {
-  //   // mouse event
-  //   document.addEventListener("mousemove", (e) => {
-  //     console.log(e.x, e.y);
-  //     setX(e.x);
-  //     setY(e.y);
-  //   });
-  // }, []);
+import React from "react";
 
-  // const styling = {
-  //   "--x": x + "px",
-  //   "--y": y + "px",
-  // };
-  return (
-    <div>
-      <section className={styles.section}>
-        <h2>Woke up and let's live</h2>
-      </section>
-      {/*@ts-ignore */}
-      <div style={styling} className={styles.light}></div>
-    </div>
-  );
+export default function TestPage() {
+  return <div>TestPage</div>;
 }
